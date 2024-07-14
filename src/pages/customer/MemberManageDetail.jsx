3dff645537dbd2146cd90f96435db41d05a8ce26
soo01234 <kwonsoohyun97@gmail.com>
2024-07-15 05:54:16 +0900

    KAN-176 refactor: 회원 회원상세조회 주소지조회 합치기

--- src/pages/customer/MemberManageDetail.jsx
+++ src/pages/customer/MemberManageDetail.jsx
@@ -1,105 +1,102 @@
-import { Flex, Tag, DatePicker, Form, Input, Button, Table } from 'antd';
+import { Flex, Tabs, Space, Form, Input, Button, Table } from 'antd';
 import moment from 'moment';
 import { LeftOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const { TabPane } = Tabs;
 
 const MemberManageDetail = () => {
   
-  const location = useLocation();
-  const [memberId, setMemberId] = useState('');
-  const [memberName, setMemberName] = useState('');
-  const [memberGender, setMemberGender] = useState('');
-  const [memberPhone, setMemberPhone] = useState('');
-  const [memberEmail, setMemberEmail] = useState('');
-  const navigate = useNavigate();
+  // const location = useLocation();
+  // const [memberId, setMemberId] = useState('');
+  // const [memberName, setMemberName] = useState('');
+  // const [memberGender, setMemberGender] = useState('');
+  // const [memberPhone, setMemberPhone] = useState('');
+  // const [memberEmail, setMemberEmail] = useState('');
 
-  useEffect(() => {
-    setMemberId(location.state?.selectedMemberId || '');
-    setMemberName(location.state?.selectedMemberName || '');
-    setMemberGender(location.state?.selectedMemberGender || '');
-    setMemberPhone(location.state?.selectedMemberPhone || '');
-    setMemberEmail(location.state?.selectedMemberEmail || '');
-  }, [location.state]);
+  const navigate = useNavigate();
+  const [customer, setCustomer] = useState(null);
+  const [addresses, setAddresses] = useState([]);
 
   const onHandleBackClick = () => {
     navigate(-1); // 이전 페이지로 이동
   };
 
+  // useEffect(() => {
+  //   setMemberId(location.state?.selectedMemberId || '');
+  //   setMemberName(location.state?.selectedMemberName || '');
+  //   setMemberGender(location.state?.selectedMemberGender || '');
+  //   setMemberPhone(location.state?.selectedMemberPhone || '');
+  //   setMemberEmail(location.state?.selectedMemberEmail || '');
+  // }, [location.state]);
+
+
+
+  // useEffect(() => {
+  //   axios.get(`http://localhost:3001/member/${customerId}`)
+  //     .then(res => setCustomer(res.data))
+  //     .catch(err => console.error(err));
+
+  //   axios.get(`http://localhost:3001/address?member_id=${customerId}`)
+  //     .then(res => setAddresses(res.data))
+  //     .catch(err => console.error(err));
+  // }, [customerId]);
+
+  // const onHandleBackClick = () => {
+  //   navigate(-1); // 이전 페이지로 이동
+  // };
+
 
   return (
     <div>
-      <Flex gap="small" justify='flex-start'> 
-        <Flex gap="small" wrap>
-        <LeftOutlined  onClick={onHandleBackClick}/>
-        </Flex>
-        <Flex gap="small" wrap>
-          <h2>회원상세정보</h2>
-        </Flex>
+      <Flex gap="small" justify="flex-start">
+        <LeftOutlined onClick={onHandleBackClick} />
+        <h2>회원 상세 정보</h2>
       </Flex>
-      
-      <Flex 
-        gap="5rem" 
-        vertical>
-        <Flex gap="3rem">
-          <Form>
-            <h3>회원아이디</h3>
-            <Input disabled placeholder={memberId}  />
-          </Form>
-          <Form>
-            <h3>회원명</h3>
-            <Input disabled placeholder={memberName}  />
-          </Form>
-          <Form>
-            <h3>성별</h3>
-            <Input disabled placeholder={memberGender}  />
-          </Form>
-        </Flex>
-        <Flex gap="3rem">
-          <Form>
-            <h3>휴대전화번호</h3>
-            <Input disabled placeholder={memberPhone}  />
-          </Form>
-          
-        </Flex>
-        <Flex gap="3rem">
-          <Form>
-            <h3>이메일</h3>
-            <Input 
-              disabled 
-              placeholder={memberEmail}
-              style={{ width: 500 }}  />
-          </Form>
-        </Flex>
-        <Flex gap="middle">
-          <Form>
-            <h3>주소</h3>
-            <Form.Item style={{ marginBottom: '1rem'}}>
-              <Input 
-                disabled 
-                placeholder="우편번호" 
-                style={{width: 200}}
+
+      <Tabs defaultActiveKey="1" items={[
+        {
+          label: '개인 정보',
+          key: '1',
+          children: customer && (
+            <Flex gap="middle" vertical>
+              <Input disabled addonBefore="회원 ID" value={customer.member_id} />
+              <Input disabled addonBefore="회원명" value={customer.member_name} />
+              <Input disabled addonBefore="성별" value={customer.gender} />
+              <Input disabled addonBefore="휴대전화" value={customer.mobile_phone} />
+              <Input disabled addonBefore="이메일" value={customer.email} style={{ width: 500 }} />
+              {addresses.length > 0 && (
+                <Input disabled addonBefore="주소" value={addresses[0].address} style={{ width: 600 }} />
+              )}
+            </Flex>
+          ),
+        },
+        {
+          label: '주소 정보',
+          key: '2',
+          children: (
+            <Flex gap="middle" vertical>
+              {addresses.map((address, index) => (
+                <Input
+                  key={address.id}
+                  disabled
+                  addonBefore={`주소지 ${index + 1}`}
+                  value={address.address}
+                  style={{ width: 600 }}
                 />
-            </Form.Item>
-            <Form.Item style={{ marginBottom: '1rem' }}>
-              <Input 
-                disabled 
-                placeholder="주소" 
-                style={{width: 600}}
-                />
-            </Form.Item>
-            <Form.Item>
-              <Input 
-                disabled 
-                placeholder="상세주소" 
-                style={{width: 600}}
-                />
-            </Form.Item>
-          </Form>
-        </Flex>
-      </Flex>
-        
+              ))}
+            </Flex>
+          ),
+        },
+      ]} />
+
+      <Space style={{ marginTop: 16 }}>
+        <Button onClick={onHandleBackClick}>목록</Button>
+        {/* 수정, 삭제 등 추가 버튼 */}
+      </Space>
     </div>
   )
 }
