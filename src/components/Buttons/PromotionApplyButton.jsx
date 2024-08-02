888435687083d12c325bb5a8cdb36c85221b163b
soo01234 <kwonsoohyun97@gmail.com>
2024-08-03 05:27:23 +0900

    KAN-691 style: 식품 등록 및 신청 버튼 색 변경

--- src/components/Buttons/PromotionApplyButton.jsx
+++ src/components/Buttons/PromotionApplyButton.jsx
@@ -5,8 +5,9 @@ const PromotionApplyButton = ({title, onClick}) => {
   return (
     <div>
       <Button 
+        // type='primary'
         icon={<ThunderboltOutlined  style={{fontWeight: 'bold' }}/>}
-        style={{width: '110px', height: '55px', backgroundColor: '#B3E0F2'}} 
+        style={{width: '110px', height: '55px', backgroundColor: '#FFFFFF', color: '#FFB000', borderColor: '#FFB000'}} 
         onClick={onClick}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
           <span style={{ fontSize: '13px', fontWeight: 'bold' }}>{title}</span> 
